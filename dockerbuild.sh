953b7c99205c40782f4b7a9ead549f79ff2e02a2
jferlez <jferlez@gmail.com>
2022-03-05 10:35:55 -0800

    Remove echo test; make dockerrun.sh executable

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -3,4 +3,4 @@
 user=`id -n -u`
 GID=`id -g`
 
-echo "docker build --build-arg USER_NAME=$user --build-arg UID=$UID --build-arg GID=$GID -t fastbatllnn-test:${user} ."
+docker build --build-arg USER_NAME=$user --build-arg UID=$UID --build-arg GID=$GID -t fastbatllnn-test:${user} .
