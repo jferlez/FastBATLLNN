473d84a705101f1a05e8316e6321bde08591bc3d
jferlez <jferlez@gmail.com>
2022-03-05 17:31:29 -0800

    Include detection of authorized_keys

--- dockerrun.sh
+++ dockerrun.sh
@@ -3,5 +3,19 @@
 user=`id -n -u`
 uid=`id -u`
 gid=`id -g`
-mkdir container_results
+if [ ! -d container_results ]
+then
+    mkdir container_results
+fi
+cd container_results
+if [ -e ~/.ssh/id_rsa.pub ]
+then
+    cat ~/.ssh/id_rsa.pub > authorized_keys
+    echo "\n" >> authorized_keys
+fi
+if [ -e ~/.ssh/authorized_keys ]
+then
+    cat ~/.ssh/authorized_keys >> authorized_keys
+fi
+cd ..
 docker run --privileged -it -p 3000:22 -v "$(pwd)"/container_results:/home/${user}/results fastbatllnn-test:${user} ${user}
